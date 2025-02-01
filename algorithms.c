a1797ce8f12ffd8b9f9057f537ce56825a2e178c
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-01-31 19:37:33 -0700

    Add functionality to insertion, swap and print functions
    The insertion function is the one seen in class where the
    complexity is O(n) for the best case scenario and O(n^2)
    for the worst case scenario.

--- algorithms.c
+++ algorithms.c
@@ -5,9 +5,50 @@ void print_array(int*, size_t);
 
 int main() 
 {
-    int array[] = {7,3,2,1,0};
+    int array[] = {10,3,2,1,0};
     int size = sizeof(array) / sizeof(array[0]);
     insertion_sort(array, size);
     print_array(array, size);
     return 0;
+}
+
+/*
+function insertionSort(array):
+    for i from 1 to length(array):
+        key = array[i]  // Store current element
+        j = i - 1  // Index of the element to compare with
+        while j >= 0 and array[j] > key:
+            array[j + 1] = array[j]  // Shift elements to the right
+            j = j - 1
+        array[j + 1] = key  // Insert key at the correct position
+*/
+
+void insertion_sort(int* array, size_t size) 
+{
+    int key, j;
+    for(int i = 1; i < size; i++) {
+        key = array[i];
+        j = i - 1;
+        while (j >= 0 && array[j] > key) {
+            swap(&array[j+1], &array[j]);
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+}
+
+void swap(int* m, int* n) 
+{
+    int temp = *m;
+    *m = *n;
+    *n = temp;
+}
+
+void print_array(int* m, size_t size)
+{
+    printf("[");
+    for(int i = 0; i < size-1; i++) {
+        printf("%d, ", m[i]);
+    }
+    printf("%d]",m[size-1]);
 }
